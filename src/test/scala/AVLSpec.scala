8d1b45408c04f7d037c707bb66fd1ef795594057
gala377 <r.galczak@gmail.com>
2018-05-31 06:03:40 +0200

    merge. min, max, intersction tests

--- src/test/scala/AVLSpec.scala
+++ src/test/scala/AVLSpec.scala
@@ -104,12 +104,104 @@ class AVLSpec extends FlatSpec {
 
   }
 
+  it should "throw if calling max" in {
+    val tree = new AVL[Int]
+    assertThrows[RuntimeException](tree.max())
+  }
+
+  it should "throw if calling min" in {
+    val tree = new AVL[Int]
+    assertThrows[RuntimeException](tree.min())
+  }
+
+  it should "result in an empty intersection" in {
+    val tree = new AVL[Int]
+    val tree_2 = new AVL[Int].add(1).add(2).add(3)
+    val root = tree.intersection(tree_2).getRoot()
+    root match {
+      case Some(_) => fail("root should be empty")
+      case None => assert(true)
+    }
+  }
+
+  it should "contain all values from second tree after merge" in {
+    val tree = new AVL[Int]
+    val tree_2 = new AVL[Int].add(1).add(2).add(3)
+    val final_tree = tree.merge(tree_2)
+    assert(final_tree.find(1))
+    assert(final_tree.find(2))
+    assert(final_tree.find(3))
+  }
+
   "Non empty AVLTree" should "remove a value from itself" in {
     val tree = new AVL[Int].add(5).add(7).add(3).add(1).add(9).add(10)
     val new_tree = tree.remove(3)
     assert(!new_tree.find(3))
   }
 
+  it should "find a maximum value" in {
+    val tree = new AVL[Int].add(5).add(7).add(3).add(1).add(9).add(10)
+    assert( tree.max() == 10 )
+  }
 
+  it should "find minimum value" in {
+    val tree = new AVL[Int].add(5).add(7).add(3).add(1).add(9).add(10)
+    assert( tree.min() == 1 )
+  }
+
+  it should "contain values from both trees after merge" in {
+    val tree = new AVL[Int].add(5).add(1)
+    val tree2 = new AVL[Int].add(2).add(7)
+    val final_tree = tree.merge(tree2)
+    assert(final_tree.find(1))
+    assert(final_tree.find(5))
+    assert(final_tree.find(7))
+    assert(final_tree.find(2))
+  }
+
+  it should "not double values in merge" in {
+    val tree = new AVL[Int].add(5).add(7)
+    val tree2 = new AVL[Int].add(2).add(7)
+    val final_tree = tree.merge(tree2)
+    assert(final_tree.height() == 1)
+  }
+
+  it should "be balanced after merge" in {
+    val tree = new AVL[Int].add(5).add(7).add(89).add(2).add(234).add(67)
+    val tree2 = new AVL[Int].add(2).add(1).add(3).add(4).add(8).add(9)
+    val final_tree = tree.merge(tree2)
+    assert(final_tree.balance() < 2 && final_tree.balance() > -2)
+  }
+
+  it should "contain only values present in both trees after intersection" in {
+    val tree = new AVL[Int].add(2).add(1).add(5)
+    val tree2 = new AVL[Int].add(2).add(1).add(3)
+    val final_tree = tree.intersection(tree2)
+    assert(final_tree.find(2))
+    assert(final_tree.find(1))
+    assert(!final_tree.find(5))
+    assert(!final_tree.find(3))
+    assert(final_tree.find(2))
+    assert(final_tree.find(1))
+    assert(!final_tree.find(5))
+    assert(!final_tree.find(3))
+  }
+
+  it should "be empty if no common values are present" in {
+    val tree = new AVL[Int].add(21).add(12).add(5)
+    val tree2 = new AVL[Int].add(2).add(1).add(3)
+    val root = tree.intersection(tree2).getRoot()
+    root match {
+      case Some(_) => fail("root should be empty")
+      case None => assert(true)
+    }
+  }
+
+  it should "be balanced after intersection" in {
+    val tree = new AVL[Int].add(1).add(2).add(3).add(4).add(7)
+    val tree2 = new AVL[Int].add(1).add(2).add(3).add(4).add(7)
+    val final_tree = tree.intersection(tree2)
+    assert(final_tree.balance() < 2 && final_tree.balance() > -2)
+  }
 }
 
